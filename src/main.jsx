0fc613ca8aa2314226302c5f71a4c020f2e301e2
kramakrishnan@cloudflare.com <rkarthik.iamok@gmail.com>
2025-03-29 21:11:01 -0400

    Completed the Vanilla JS apps for React

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <div>
-    <h1 className="text-3xl font-bold underline">Hello Vite!</h1>
-  </div>
-);
+root.render(<App />);
